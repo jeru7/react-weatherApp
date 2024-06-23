215a8287319771a0e14f984d6e74e125dd4c1020
jeru7 <john.ungab7@gmail.com>
2024-06-24 02:16:00 +0800

    add: api for day/hour forecast

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -22,3 +22,21 @@ export const GetCurrentWeather = async (city, country) => {
     console.log(`Error in fetching current weather. ${e.message}`)
   }
 }
+
+export const DaysForecast = async (lat, lon) => {
+  try {
+    const response = await axios.get(
+      'https://api.openweathermap.org/data/2.5/forecast',
+      {
+        params: {
+          q: lat,
+          lon,
+          appid: API_KEY,
+        },
+      }
+    )
+    return response.data
+  } catch (e) {
+    console.log(`Error in fetching the day/hour forecast. ${e.message}`)
+  }
+}
