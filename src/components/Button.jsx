d011ffcd3dfcedc54121943f9d9b5a1656032702
jeru7 <john.ungab7@gmail.com>
2024-06-27 09:52:48 +0800

    Updates

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({ className, onClick, children }) {
-  return (
-    <button className={`btn ${className}`} onClick={onClick}>
-      {children}
-    </button>
-  )
-}
-
-export default Button
--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className='fixed z-50 flex items-center justify-center w-full h-full opacity-90 main-bg'>
-      <div className='loader'></div>
-    </div>
-  )
-}
-
-export default Loader
